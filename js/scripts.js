d102dd778d436cbba351e943fc16bc2e34f4f6e0
saiyanka <saiyanka@gmail.com>
2017-04-17 23:32:51 +0300

    readme

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,29 @@
 $(document).ready(function() {
-  $("#userInput form").submit(function(event) {
+    $("#userInput form").submit(function(event) {
 
-    var arr = [];
-    var num = parseInt($("input#number").val());
+        var arr = [];
+        var num = parseInt($("input#number").val());
 
-    for(var i=1; i<=num; i++){
-      arr.push([i]);
-    }
+        for (var i = 1; i <= num; i++) {
+            arr.push([i]);
+        }
 
-    for(var j = 0; j < arr.length; j++){
+        for (var j = 0; j < arr.length; j++) {
 
-      if (arr[j]%3 === 0 && arr[j]%5 === 0) {
-        $(".result").append("<li>PingPong</li>");
-      }
-      else if (arr[j]%5 === 0) {
-        $(".result").append("<li>Pong</li>");
-      }
-      else if (arr[j]%3 === 0){
-        $(".result").append("<li>Ping</li>");
-      }
-      else {
-        $(".result").append("<li>"+arr[j]+"</li>");
-      }
-    }
+            if (arr[j] % 3 === 0 && arr[j] % 5 === 0) {
+                $(".result").append("<li>PingPong</li>");
+            } else if (arr[j] % 5 === 0) {
+                $(".result").append("<li>Pong</li>");
+            } else if (arr[j] % 3 === 0) {
+                $(".result").append("<li>Ping</li>");
+            } else {
+                $(".result").append("<li>" + arr[j] + "</li>");
+            }
+        }
 
-    $(".footer").show();
-    // $("#output").show();
+        $(".footer").show();
+        // $("#output").show();
 
-    event.preventDefault();
-  });
+        event.preventDefault();
+    });
 });
