d173727ead5aa8195b5873eab63f13ebd4519413
saiyanka <saiyanka@gmail.com>
2017-04-17 22:44:00 +0300

    cant figure out function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+
+function myFunction (){
+  var greeting;
+if (i%3===0){
+  greeting="ping";
+} else if (i%5===0){
+  greeting="pong";
+}else if (i%15===0) {
+  greeting="pingpong";
+} else {
+
+  console.log(myFunction);
+}
+ myFunction();
+}
